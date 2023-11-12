9d0655224674492c85c54eb85f1ffc88112d7ff8
tobiwnrt <batiix.weinert@gmail.com>
2023-11-12 22:19:22 +0100

    feat(Carousel): rotate by scroll and arrow keys

--- scripts/main.js
+++ scripts/main.js
@@ -26,32 +26,17 @@ camera.position.set(0, 0, 20);
 //const controls = new OrbitControls(camera, renderer.domElement);
 
 // helpers
-const axisHelper = new THREE.AxesHelper(50);
-scene.add(axisHelper);
+const axisHelper = new THREE.AxesHelper(5000);
+//scene.add(axisHelper);
 // const gridHelper = new THREE.GridHelper( 10, 10 );
 // scene.add( gridHelper );
-
-// geometry
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
-
-const exPlaneGeometry = new THREE.PlaneGeometry(5, 5, 32);
-const exPlaneMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  side: THREE.DoubleSide,
-});
-const exPlane = new THREE.Mesh(exPlaneGeometry, exPlaneMaterial);
-exPlane.rotation.x = Math.PI / 2;
-scene.add(exPlane);
 
 // we want a carousel of 5 planes that rotate around the y axis
 const carousel = new THREE.Group();
 scene.add(carousel);
 
 // Define the geometry and material for the planes
-const roundedBoxGeometry = new RoundedBoxGeometry(10, 16, 0.7, 3, 0.5);
+const roundedBoxGeometry = new RoundedBoxGeometry(10, 16, 0.5, 3, 0.5);
 const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff];
 
 // create 5 boxes and add them to the carousel
@@ -74,6 +59,99 @@ for (let i = 0; i < 5; i++) {
   carousel.add(roundedBox);
 }
 
+// text
+// const fontLoader = new FontLoader();
+// fontLoader.load(
+//   "./assets/fonts/droid_serif_regular.typeface.json",
+//   (droidFont) => {
+//     const textGeometry = new TextGeometry("hello world", {
+//       height: 0.2,
+//       size: 1,
+//       font: droidFont,
+//     });
+//     const textMaterial = new THREE.MeshNormalMaterial();
+//     const text = new THREE.Mesh(textGeometry, textMaterial);
+//     text.position.set(-10, 0, 0);
+//     scene.add(text);
+//   }
+// );
+
+// animation
+let isDragging = false;
+let isTransitioning = false;
+let previousPointerX = 0;
+
+document.addEventListener("pointerdown", onPointerDown);
+document.addEventListener("pointermove", onPointerMove);
+document.addEventListener("pointerup", onPointerUp);
+document.addEventListener("wheel", onPointerMove);
+// add event for arrow left and right
+document.addEventListener("keydown", (event) => {
+  let targetRotation = carousel.rotation.y;
+  if (event.key === "ArrowLeft") {
+    targetRotation += Math.PI * 2 * (1 / 5);
+  }
+  if (event.key === "ArrowRight") {
+    targetRotation -= Math.PI * 2 * (1 / 5);
+  }
+  new TWEEN.Tween(carousel.rotation)
+    .to({ y: targetRotation }, 800)
+    .easing(TWEEN.Easing.Bounce.Out)
+    .onComplete(() => {
+      isTransitioning = false;
+    })
+    .onStart(() => {
+      isTransitioning = true;
+    })
+    .onStop(() => {
+      isTransitioning = false;
+    })
+    .start();
+});
+
+function onPointerDown(event) {
+  isDragging = true;
+  previousPointerX = event.clientX || event.touches[0].clientX;
+}
+
+function onPointerMove(event) {
+  if (isTransitioning) return;
+  if (event.type === "wheel") {
+    // if the user scrolls, we need to rotate the carousel all the way to the next box
+    const scrollThreshold = 10;
+    let targetRotation = carousel.rotation.y;
+    if (event.deltaY < -scrollThreshold) {
+      targetRotation += Math.PI * 2 * (1 / 5);
+    } else if (event.deltaY > scrollThreshold) {
+      targetRotation -= Math.PI * 2 * (1 / 5);
+    }
+    new TWEEN.Tween(carousel.rotation)
+      .to({ y: targetRotation }, 800)
+      .easing(TWEEN.Easing.Bounce.Out)
+      .onComplete(() => {
+        isTransitioning = false;
+      })
+      .onStart(() => {
+        isTransitioning = true;
+      })
+      .onStop(() => {
+        isTransitioning = false;
+      })
+      .start();
+
+    // Start the animation loop
+    animate();
+  }
+  if (isDragging) {
+    const currentPointerX = event.clientX || event.touches[0].clientX;
+    const deltaX = currentPointerX - previousPointerX;
+    // rotation speed multiplier
+    carousel.rotation.y += deltaX * 0.005;
+    previousPointerX = currentPointerX;
+    renderer.render(scene, camera);
+  }
+}
+
 const snapAngles = [];
 for (let i = 0; i < 5; i++) {
   snapAngles.push((i / 5) * Math.PI * 2 + Math.PI * 0.7);
@@ -91,52 +169,11 @@ function findClosestSnapAngle(currentAngle) {
       closestAngleDifference = angleDifference;
     }
   }
-  //("curr, close", currentAngle, closestSnapAngle);
   return closestSnapAngle;
 }
 
-// text
-const fontLoader = new FontLoader();
-fontLoader.load(
-  "./assets/fonts/droid_serif_regular.typeface.json",
-  (droidFont) => {
-    const textGeometry = new TextGeometry("hello world", {
-      height: 0.2,
-      size: 1,
-      font: droidFont,
-    });
-    const textMaterial = new THREE.MeshNormalMaterial();
-    const text = new THREE.Mesh(textGeometry, textMaterial);
-    text.position.set(-10, 0, 0);
-    scene.add(text);
-  }
-);
-
-// animation
-let isDragging = false;
-let previousPointerX = 0;
-
-document.addEventListener("pointerdown", onPointerDown);
-document.addEventListener("pointermove", onPointerMove);
-document.addEventListener("pointerup", onPointerUp);
-
-function onPointerDown(event) {
-  isDragging = true;
-  previousPointerX = event.clientX || event.touches[0].clientX;
-}
-
-function onPointerMove(event) {
-  if (isDragging) {
-    const currentPointerX = event.clientX || event.touches[0].clientX;
-    const deltaX = currentPointerX - previousPointerX;
-    // rotation speed multiplier
-    carousel.rotation.y += deltaX * 0.005;
-    previousPointerX = currentPointerX;
-    renderer.render(scene, camera);
-  }
-}
-
 function onPointerUp() {
+  if (isTransitioning) return;
   if (isDragging) {
     isDragging = false;
 
@@ -156,13 +193,18 @@ function onPointerUp() {
     }
     const snapAngle = findClosestSnapAngle(carousel.rotation.y);
     new TWEEN.Tween(carousel.rotation)
-      .to({ y: snapAngle }, 300) // You can adjust the duration as needed
-      .easing(TWEEN.Easing.Back.Out) // You can change the easing function
+      .to({ y: snapAngle }, 300)
+      .easing(TWEEN.Easing.Back.Out)
       .onComplete(() => {
-        console.log("done");
+        isTransitioning = false;
+      })
+      .onStart(() => {
+        isTransitioning = true;
+      })
+      .onStop(() => {
+        isTransitioning = false;
       })
       .start();
-    //carousel.rotation.y = snapAngle;
   }
 }
 
