be11695aa6c7221396d270e7290d64108908a9af
tobiwnrt <batiix.weinert@gmail.com>
2023-12-11 21:00:00 +0100

    chore(StarryNight): hide starry night

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ import {
   initCameraShot,
 } from "./inits.js";
 import { initCarousel } from "./carousel.js";
+import { initItem } from "./item.js";
 import { toSlideWelcome } from "./slides/slideWelcome.js";
 import { toSlideAbout } from "./slides/slideAbout.js";
 import { toSlideContact } from "./slides/slideContact.js";
@@ -17,9 +18,8 @@ import "./events.js";
 initThree();
 initCarousel();
 initBloom();
-initStarryNight();
+//initStarryNight();
 await initCameraShot();
-console.log(globals.isDarkReaderEnabled);
 
 // manually trigger the initial slide
 switch (globals.devOptions.initialSlide) {
@@ -46,18 +46,18 @@ function animate() {
   TWEEN.update();
   globals.composer.render();
   // add movement to starry night using mouse position
-  globals.stars.rotation.y = globals.mouseX * 0.1;
-  globals.stars.rotation.x = globals.mouseY * 0.1;
+  // globals.stars.rotation.y = globals.mouseX * 0.1;
+  // globals.stars.rotation.x = globals.mouseY * 0.1;
 
-  // make random stars twinkle
-  const time = Date.now() * 0.00005;
-  globals.stars.geometry.attributes.position.array.forEach((_, index) => {
-    if (index % 3 === 0) {
-      globals.stars.geometry.attributes.position.array[index] +=
-        Math.sin(index + time) * 3;
-    }
-  });
-  globals.stars.geometry.attributes.position.needsUpdate = true;
+  // // make random stars twinkle
+  // const time = Date.now() * 0.00005;
+  // globals.stars.geometry.attributes.position.array.forEach((_, index) => {
+  //   if (index % 3 === 0) {
+  //     globals.stars.geometry.attributes.position.array[index] +=
+  //       Math.sin(index + time) * 3;
+  //   }
+  // });
+  // globals.stars.geometry.attributes.position.needsUpdate = true;
 }
 
 animate();
